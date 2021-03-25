c2acd1fbe60d5876ff53495d4932f7f5114b4b6d
Vladiuse <vladiuse@gmail.com>
2021-03-26 00:36:54 +0300

    calculator 3ku

---
+++ calculator.py
@@ -0,0 +1,53 @@
+# https://www.codewars.com/kata/5235c913397cbf2508000048
+
+class Calculator(object):
+    def evaluate(self, string):
+        def resolve(array):
+            operators_1 = ['/', '*']
+            operators_2 = ['+', '-']
+            while '/' in array or '*' in array:
+                for char in array:
+                    if char in operators_1:
+                        oper = char
+                        pos = array.index(oper)
+                        a, b = array[pos - 1], array[pos + 1]
+                        if oper == '/':
+                            array[pos - 1:pos + 2] = [float(a) / float(b)]
+                            break
+                        elif oper == '*':
+                            array[pos - 1:pos + 2] = [float(a) * float(b)]
+                            break
+            while '+' in array or '-' in array:
+                for char in array:
+                    if char in operators_2:
+                        oper = char
+                        pos = array.index(oper)
+                        a, b = array[pos - 1], array[pos + 1]
+                        if oper == '-':
+                            array[pos - 1:pos + 2] = [float(a) - float(b)]
+                            break
+                        elif oper == '+':
+                            array[pos - 1:pos + 2] = [float(a) + float(b)]
+                            break
+            return str(array[0])
+
+        def get_bracket(array):
+            start_pos = 0
+            start = end = None
+            for id, char in enumerate(array):
+                if char == '(':
+                    start_pos = id
+                if char == ')':
+                    start = start_pos
+                    end = id
+                    break
+            rez = resolve(array[start + 1:end])
+            array[start:end + 1] = [rez]
+            return array
+
+        string = string.split(' ')
+        while '(' in string:
+            string = get_bracket(string)
+        string = resolve(string)
+        string = float(string)
+        return string
