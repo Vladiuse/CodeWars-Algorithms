91a80e853d5b22f54f08a970a3d1181ce9b5fb0c
Vladiuse <vladiuse@gmail.com>
2021-04-04 13:14:28 +0300

    fix spiral

--- spiral.py
+++ spiral.py
@@ -1,6 +1,12 @@
-# def spiralize(size):
-#     spiral = [[0] * size] * size
-#     return spiral
+# https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+# make = [[1, 1, 1, 1, 1, 1, 1],
+#        [0, 0, 0, 0, 0, 0, 1],
+#        [1, 1, 1, 1, 1, 0, 1],
+#        [1, 0, 0, 0, 1, 0, 1],
+#        [1, 0, 1, 1, 1, 0, 1],
+#        [1, 0, 0, 0, 0, 0, 1],
+#        [1, 1, 1, 1, 1, 1, 1]]
+
 def round_array(arr, number):
     new = []
     arr = arr.copy()
@@ -8,10 +14,10 @@ def round_array(arr, number):
     plus_li = [number] * (length + 2)
     for i in arr:
         if isinstance(i, int):
-            new.append([number,i,number])
+            new.append([number, i, number])
             break
         li = i.copy()
-        li.insert(0,number)
+        li.insert(0, number)
         li.append(number)
         new.append(li)
     new.append(plus_li)
@@ -22,37 +28,25 @@ def round_array(arr, number):
         new[1][0] = 0
     return new
 
+
 def spiralize(size):
-    x = size % 4
-    if x == 1:
-        spiral = [[1]]
-        start_number = 0
-    if x == 3:
-        spiral = [[0]]
-        start_number = 1
-    if x == 2:
-        spiral = [[1,1], [0,1]]
-        start_number = 0
-    if x == 0:
-        spiral = [[0,0], [0,0]]
-        start_number = 1
+    center = size % 4
+    dic = {1: {'spiral': [[1]], 'start_number': 0},
+           2: {'spiral': [[1, 1], [0, 1]], 'start_number': 0},
+           3: {'spiral': [[0]], 'start_number': 1},
+           0: {'spiral': [[0, 0], [0, 0]], 'start_number': 1},
+           }
+    spiral = dic[center]['spiral']
+    start_number = dic[center]['start_number']
     while len(spiral) < size:
-        spiral = round_array(spiral,start_number)
+        spiral = round_array(spiral, start_number)
         if start_number == 0:
             start_number = 1
         else:
             start_number = 0
     return spiral
-rez = spiralize(5)
-for i in rez:
-    print(i)
-
-rez = spiralize(6)
-for i in rez:
-    print(i)
 
 
 rez = spiralize(7)
 for i in rez:
     print(i)
-
